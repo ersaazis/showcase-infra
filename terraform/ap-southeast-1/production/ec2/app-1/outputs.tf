ae0a627a1271e83db4e8d3a81b3574f574ee1a3f
ersaazis <eam24maret@gmail.com>
2025-05-12 23:40:59 +0700

    update production terraform

--- terraform/ap-southeast-1/production/ec2/app-1/outputs.tf
+++ terraform/ap-southeast-1/production/ec2/app-1/outputs.tf
@@ -8,6 +8,11 @@ output "private_ip" {
   value       = aws_instance.ec2.private_ip
 }
 
+output "public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.ec2.public_ip
+}
+
 output "security_group_id" {
   description = "The ID of the EC2 security group"
   value       = aws_security_group.ec2_sg.id
--- terraform/ap-southeast-1/production/vpc-peering/vpc-peering-production-data-production/output.tf
+++ terraform/ap-southeast-1/production/vpc-peering/vpc-peering-production-data-production/output.tf
@@ -5,13 +5,25 @@ output "data_production_to_production_peering_id" {
 }
 
 # Output Route Table ID di Control-Plane untuk peering ke Production
-output "data_production_to_production_route_table_id" {
-  value = aws_route.data_production_to_production.route_table_id
+output "data_production_private_to_production_route_table_id" {
+  value = aws_route.data_production_private_to_production.route_table_id
   description = "The Route Table ID in Control-Plane for the peering route to Production"
 }
 
 # Output Route Table ID di Production untuk peering ke Control-Plane
-output "production_to_data_production_route_table_id" {
-  value = aws_route.production_to_data_production.route_table_id
+output "production_private_to_data_production_route_table_id" {
+  value = aws_route.production_private_to_data_production.route_table_id
+  description = "The Route Table ID in Production for the peering route to Control-Plane"
+}
+
+# Output Route Table ID di Control-Plane untuk peering ke Production
+output "data_production_public_to_production_route_table_id" {
+  value = aws_route.data_production_public_to_production.route_table_id
+  description = "The Route Table ID in Control-Plane for the peering route to Production"
+}
+
+# Output Route Table ID di Production untuk peering ke Control-Plane
+output "production_public_to_data_production_route_table_id" {
+  value = aws_route.production_public_to_data_production.route_table_id
   description = "The Route Table ID in Production for the peering route to Control-Plane"
 }
